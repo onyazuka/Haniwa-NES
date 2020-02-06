07864b60e9bab576688d291a8ad8f85b3999f295
onyazuka <onizuka2008@yandex.ru>
2020-02-06 09:57:06 +0300

    Added CPU memory mirroring

--- core/memory.cpp
+++ core/memory.cpp
@@ -3,3 +3,40 @@
 // - value-initializing memory(init with zeros)
 Memory::Memory()
     : memory{} {}
+
+u8 Memory::read8(Address offset) {
+    Address fixedAddress = _mirrorAddressFix(offset);
+    return memory[fixedAddress];
+}
+
+Memory& Memory::write8(Address offset, u8 val) {
+    Address fixedAddress = _mirrorAddressFix(offset);
+    memory[fixedAddress] = val;
+    return *this;
+}
+
+u16 Memory::read16(Address offset) {
+    Address fixedAddress = _mirrorAddressFix(offset);
+    return memory[fixedAddress] + (memory[fixedAddress + 1] << 8);
+}
+
+Memory& Memory::write16(Address offset, u16 val) {
+    Address fixedAddress = _mirrorAddressFix(offset);
+    memory[fixedAddress] = (u8)(val & 0xff);
+    memory[fixedAddress + 1] = (u8)((val & 0xff00) >> 8);
+    return *this;
+}
+
+/*
+   Fix address, considering mirroring, for read/write operations.
+   For example, PPU registers are stored at $2000 through $2007, and mirrored from $2008 through $3FFF, so a write to $3456 is the same as a write to $2006.
+   CPU memory mirrored ranges:
+    Original: $0000-$07FF, mirrors: $0800-$1FFF;
+    Original: $2000-$2007, mirrors: $2008-$3FFF.
+*/
+Address Memory::_mirrorAddressFix(Address address) {
+    if (address < 0x2000) return address % 0x0800;
+    else if(address >= 0x2000 && address < 0x4000) return 0x2000 + address % 8;
+    // no mirroring
+    else return address;
+}
--- core/memory.hpp
+++ core/memory.hpp
@@ -5,11 +5,13 @@
 class Memory {
 public:
     Memory();
-    inline u8 read8(Address offset) { return memory[offset]; }
-    inline Memory& write8(Address offset, u8 val) { memory[offset] = val; return *this; }
-    inline u16 read16(Address offset) { return memory[offset] + (memory[offset + 1] << 8); }
-    inline Memory& write16(Address offset, u16 val) { memory[offset] = (u8)(val & 0xff); memory[offset + 1] = (u8)((val & 0xff00) >> 8);  return *this; }
+    u8 read8(Address offset);
+    Memory& write8(Address offset, u8 val);
+    u16 read16(Address offset);
+    Memory& write16(Address offset, u16 val);
 private:
+    Address _mirrorAddressFix(Address address);
+
     std::array<u8, 0x10000> memory;
 };
 
